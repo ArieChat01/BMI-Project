619af43b348f1696040cf9da8a9f2466bc66fa29
Unknown <arieauna.chatmon.2019@student.lhschools.org>
2018-08-30 14:18:28 -0400

    BMI Project

---
+++ src/MathTester.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MathTester {
+
+public static void main(String[] args) {
+
+Scanner keyboard;
+String userHeightInches;
+String userHeightFeet;
+
+
+keyboard = new Scanner(System.in);
+
+
+}
+
+
+
+}
