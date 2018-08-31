81bbe597d26db5e7b05b7872e71516d0e8e40f9d
ArieChat01 <42781378+ArieChat01@users.noreply.github.com>
2018-08-31 10:03:34 -0400

    Added declarations

--- src/MathTester.java
+++ src/MathTester.java
@@ -5,11 +5,20 @@ public class MathTester {
 public static void main(String[] args) {
 
 Scanner keyboard;
-String userHeightInches;
-String userHeightFeet;
+String greeting
+String explanation
+String userHeightInchesQuestion;
+String userHeightFeetQuestion;
+String userWeghtPoundsQuestion;
 
-
+greeting = "Hello there!";
+explanation = "This program will allow you calculate your body mass index through a few simple steps. \n Let's begin.";
+userHeightFeetQuestion = "What is you height (only the feet)?";
+userHeightInchesQuestion = "What is your height (only the inches)?";
 keyboard = new Scanner(System.in);
+  
+System.out.println(greeting);
+System.out.println(explanation);
 
 
 }
